de680d7a135d635f1d5cf941ab1df98a9506b2de
JustKillu <nightcorekillu@gmail.com>
2023-11-21 21:14:04 -0400

    Se borro codigo que no se usaba. y se implemento de manera correcta el darkmode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState,useContext } from 'react';
 import NavBar from './components/navbar';
 import SearchMovies from './components/buscarpelis';
 import MovieCarousel from './components/carusel';
 import CambioModo from './components/dark-light-mode';
+import { DarkModeContext } from './DarkModeContext';
 import './App.css'; 
 
 const App = () => {
   const [navbarQuery, setNavbarQuery] = useState('');
-  const [darkMode, setDarkMode] = useState(false);
-
-
-  useEffect(() => {
-    const isDarkMode = localStorage.getItem('darkMode') === 'true';
-    setDarkMode(isDarkMode);
-  }, []);
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-    localStorage.setItem('darkMode', !darkMode);
-  };
+  const { darkMode, toggleDarkMode } = useContext(DarkModeContext);
 
   return (
     <div>
