ddbb324a5d3e921a8e7afe191fb9c2d86ea12a17
JustKillu <nightcorekillu@gmail.com>
2023-11-21 21:15:01 -0400

    Cambios para la nav para hacerla responsive

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NavBar.css'; 
 import { NavLink } from 'react-router-dom';
 
 const NavBar = ({ darkMode, setNavbarQuery }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav className={`nav ${darkMode ? 'dark-mode' : 'light-mode'}`}>
+    <nav className={`nav ${darkMode ? 'dark-mode' : 'light-mode'} ${isOpen ? 'open' : ''}`}>
+      <button className={`hamburger ${isOpen ? 'open' : ''}`} onClick={() => setIsOpen(!isOpen)}>
+        <span>☰</span>
+      </button>
       <input
         className="input"
         type="text"
         placeholder="Buscar..."
         onChange={(e) => setNavbarQuery(e.target.value)} 
       />
-      <ul className="links">
+      <ul className={`links ${isOpen ? 'open' : ''}`}>
         <li><NavLink className="link" activeClassName="active" exact to="/">Inicio</NavLink></li>
         <li><NavLink className="link" activeClassName="active" to="/popular">Populares</NavLink></li>
         <li><NavLink className="link" activeClassName="active" to="/catalogo">Catálogo</NavLink></li>
@@ -20,6 +25,5 @@ const NavBar = ({ darkMode, setNavbarQuery }) => {
     </nav>
   );
 };
-
 
 export default NavBar;
